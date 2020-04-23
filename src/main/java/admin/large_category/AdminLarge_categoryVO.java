95464f46c27b90d14b57c8c745ecd808afcddc10
조형배 <pwjhb@naver.com>
2020-04-23 15:39:56 +0900

    Create AdminLarge_categoryVO.java

---
+++ src/main/java/admin/large_category/AdminLarge_categoryVO.java
@@ -0,0 +1,11 @@
+package com.spring.admin.large_category.vo;
+
+import lombok.Data;
+
+@Data
+public class AdminLarge_CategoryVO {
+
+	private int lc_num;
+	private String lc_name;
+	private String lc_display;
+}
