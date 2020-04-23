0f84bd0b57c186d875332300fa4c729a37e811b7
조형배 <pwjhb@naver.com>
2020-04-23 15:38:17 +0900

    Create AdminDetailService.java

---
+++ src/main/java/admin/detail/AdminDetailService.java
@@ -0,0 +1,10 @@
+package com.spring.admin.detail.service;
+
+import java.util.List;
+
+import com.spring.admin.detail.vo.AdminDetailVO;
+
+public interface AdminDetailService {
+
+	public List<AdminDetailVO> detailList(AdminDetailVO avo);
+}
